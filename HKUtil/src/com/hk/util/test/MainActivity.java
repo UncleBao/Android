00ff753fe006498b33a8bb7d0bee83d1230f7b78
UncleBao <zheyiq@126.com>
2014-10-22 16:47:03 +0800

    工具包

---
+++ HKUtil/src/com/hk/util/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hk.util.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+}
